ffe84cd9efdc59ec8ecc0c7766ca02fc8e443465
seekpan <lixin_qiu@163.com>
2024-01-10 23:45:11 +0800

    注册用户信息骨架

---
+++ online-taxi-public/service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/ServicePassengerUserApplication.java
@@ -0,0 +1,18 @@
+package com.mashibing.servicepassengeruser;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("com.mashibing.servicepassengeruser.mapper")
+public class ServicePassengerUserApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServicePassengerUserApplication.class, args);
+    }
+
+}
