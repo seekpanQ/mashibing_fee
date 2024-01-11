fcab4c380aafcb0a87f9ceab8e9d02c24d159b66
seekpan <lixin_qiu@163.com>
2024-01-11 20:36:46 +0800

    校验token

--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/interceptor/InterceptorConfig.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/interceptor/InterceptorConfig.java
@@ -1,5 +1,7 @@
 package com.mashibing.apipassenger.interceptor;
 
+import com.mashibing.internalcommon.util.JwtUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
+    @Bean
+    public JwtInterceptor jwtInterceptor() {
+        return new JwtInterceptor();
+    }
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new JwtInterceptor())
+        registry.addInterceptor(jwtInterceptor())
                 // 拦截的路径
                 .addPathPatterns("/**")
                 // 不拦截的路径
