c6d5b9126cb0b33739e6e9c2d16d5cdf55769d0d
seekpan <lixin_qiu@163.com>
2024-01-11 16:53:24 +0800

    添加token拦截器

---
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/interceptor/JwtInterceptor.java
@@ -0,0 +1,15 @@
+package com.mashibing.apipassenger.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JwtInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return false;
+    }
+}
