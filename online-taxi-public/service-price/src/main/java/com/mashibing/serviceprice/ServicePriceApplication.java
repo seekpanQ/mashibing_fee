d4ed3072913427837846376022a1491fbbdcaedd
seekpan <lixin_qiu@163.com>
2024-01-13 10:32:59 +0800

    service-price骨架编写

---
+++ online-taxi-public/service-price/src/main/java/com/mashibing/serviceprice/ServicePriceApplication.java
@@ -0,0 +1,15 @@
+package com.mashibing.serviceprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServicePriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePriceApplication.class, args);
+    }
+
+}
