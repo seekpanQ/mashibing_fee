abd1530d4be682077326483008c4cc7bc39f7c00
seekpan <lixin_qiu@163.com>
2024-01-21 11:18:00 +0800

    service-driver-user注册到nacos

--- online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/ServiceDriverUserApplication.java
+++ online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/ServiceDriverUserApplication.java
@@ -3,9 +3,11 @@ package com.mashibing.servicedriveruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.mashibing.servicedriveruser.mapper")
+@EnableDiscoveryClient
 public class ServiceDriverUserApplication {
 
     public static void main(String[] args) {
