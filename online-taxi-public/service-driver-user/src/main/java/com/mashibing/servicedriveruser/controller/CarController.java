706214795b65056a660b8229e3f1cb8be36f6ab8
seekpan <lixin_qiu@163.com>
2024-01-22 22:43:35 +0800

    car自动生成代码

---
+++ online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/CarController.java
@@ -0,0 +1,19 @@
+package com.mashibing.servicedriveruser.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Lison
+ * @since 2024-01-22
+ */
+@RestController
+@RequestMapping("/car")
+public class CarController {
+
+}
---
+++ online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/mapper/CarMapper.java
@@ -0,0 +1,16 @@
+package com.mashibing.servicedriveruser.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mashibing.internalcommon.dto.Car;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Lison
+ * @since 2024-01-22
+ */
+public interface CarMapper extends BaseMapper<Car> {
+
+}
