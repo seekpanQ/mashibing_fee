090e8f1637e8ec7965deabb7531e39fa602e8cf1
seekpan <lixin_qiu@163.com>
2024-01-24 22:58:29 +0800

    初始化司机工作状态

--- online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverUserService.java
+++ online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverUserService.java
@@ -3,8 +3,10 @@ package com.mashibing.servicedriveruser.service;
 import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.constant.DriverCarConstants;
 import com.mashibing.internalcommon.dto.DriverUser;
+import com.mashibing.internalcommon.dto.DriverUserWorkStatus;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.servicedriveruser.mapper.DriverUserMapper;
+import com.mashibing.servicedriveruser.mapper.DriverUserWorkStatusMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,8 @@ public class DriverUserService {
 
     @Autowired
     private DriverUserMapper driverUserMapper;
+    @Autowired
+    private DriverUserWorkStatusMapper driverUserWorkStatusMapper;
 
     public ResponseResult testGetDriverUser() {
 
@@ -31,6 +35,15 @@ public class DriverUserService {
         driverUser.setGmtCreate(now);
         driverUser.setGmtModified(now);
         driverUserMapper.insert(driverUser);
+
+        // 初始化 司机工作状态表
+        DriverUserWorkStatus driverUserWorkStatus = new DriverUserWorkStatus();
+        driverUserWorkStatus.setDriverId(driverUser.getId());
+        driverUserWorkStatus.setWorkStatus(DriverCarConstants.DRIVER_WORK_STATUS_STOP);
+        driverUserWorkStatus.setGmtModified(now);
+        driverUserWorkStatus.setGmtCreate(now);
+        driverUserWorkStatusMapper.insert(driverUserWorkStatus);
+
         return ResponseResult.success("");
     }
 
