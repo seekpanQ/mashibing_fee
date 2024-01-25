818f97dc45ffa0402d3f56d49ef644ab44de65c0
seekpan <lixin_qiu@163.com>
2024-01-25 22:50:02 +0800

    api-passenger下单接受参数

---
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.mashibing.apipassenger.controller;
+
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.request.OrderRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    /**
+     * 创建订单/下单
+     *
+     * @return
+     */
+    @PostMapping("/add")
+    public ResponseResult add(@RequestBody OrderRequest orderRequest) {
+        System.out.println(orderRequest);
+        return null;
+    }
+}
