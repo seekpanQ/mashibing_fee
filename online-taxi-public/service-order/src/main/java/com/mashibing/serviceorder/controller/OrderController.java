84bd4d8a07402d60c6b1e01af978985e1a5321e5
seekpan <lixin_qiu@163.com>
2024-01-25 23:17:59 +0800

    创建service-order服务，并测试创建订单接口

--- online-taxi-public/service-order/src/main/java/com/mashibing/serviceorder/controller/OrderController.java
+++ online-taxi-public/service-order/src/main/java/com/mashibing/serviceorder/controller/OrderController.java
@@ -1,9 +1,8 @@
-package com.mashibing.apipassenger.controller;
+package com.mashibing.serviceorder.controller;
 
-import com.mashibing.apipassenger.service.OrderService;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.OrderRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
 
-    @Autowired
-    private OrderService orderService;
-
-    /**
-     * 创建订单/下单
-     *
-     * @return
-     */
     @PostMapping("/add")
     public ResponseResult add(@RequestBody OrderRequest orderRequest) {
-        System.out.println(orderRequest);
-        return orderService.add(orderRequest);
+        log.info("service-order" + orderRequest.getAddress());
+        return null;
     }
 }
