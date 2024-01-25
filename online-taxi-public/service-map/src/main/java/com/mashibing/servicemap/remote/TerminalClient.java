af45599382c68411a7f09c8e09d6d96c88775ac2
seekpan <lixin_qiu@163.com>
2024-01-25 14:17:48 +0800

    通过service-map调用高德，完成终端的创建。

--- online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/remote/TerminalClient.java
+++ online-taxi-public/service-map/src/main/java/com/mashibing/servicemap/remote/TerminalClient.java
@@ -2,7 +2,7 @@ package com.mashibing.servicemap.remote;
 
 import com.mashibing.internalcommon.constant.AmapConfigConstants;
 import com.mashibing.internalcommon.dto.ResponseResult;
-import com.mashibing.internalcommon.responese.ServiceResponse;
+import com.mashibing.internalcommon.responese.TerminalResponse;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +13,13 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
-public class ServiceClient {
+public class TerminalClient {
+
     @Value("${amap.key}")
     private String amapKey;
+
+    @Value("${amap.sid}")
+    private String amapSid;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -24,20 +28,22 @@ public class ServiceClient {
         // &key=<用户的key>
         // 拼装请求的url
         StringBuilder url = new StringBuilder();
-        url.append(AmapConfigConstants.SERVICE_ADD_URL);
+        url.append(AmapConfigConstants.TERMINAL_ADD_URL);
         url.append("?");
         url.append("key=" + amapKey);
         url.append("&");
+        url.append("sid=" + amapSid);
+        url.append("&");
         url.append("name=" + name);
-
+        log.info(url.toString());
         ResponseEntity<String> forEntity = restTemplate.postForEntity(url.toString(), null, String.class);
         String body = forEntity.getBody();
         JSONObject result = JSONObject.fromObject(body);
         JSONObject data = result.getJSONObject("data");
-        String sid = data.getString("sid");
-        ServiceResponse serviceResponse = new ServiceResponse();
-        serviceResponse.setSid(sid);
+        String tid = data.getString("tid");
+        TerminalResponse terminalResponse = new TerminalResponse();
+        terminalResponse.setTid(tid);
 
-        return ResponseResult.success(serviceResponse);
+        return ResponseResult.success(terminalResponse);
     }
 }
