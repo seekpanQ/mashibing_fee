afb54fc32694ea599d6bfa48a0dc3e2ae6240c40
seekpan <lixin_qiu@163.com>
2024-01-26 20:59:17 +0800

    service-price判断计价规则是否是最新版本及获取最新计价规则

--- online-taxi-public/service-price/src/main/java/com/mashibing/serviceprice/controller/PriceRuleController.java
+++ online-taxi-public/service-price/src/main/java/com/mashibing/serviceprice/controller/PriceRuleController.java
@@ -5,10 +5,7 @@ import com.mashibing.internalcommon.dto.PriceRule;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.serviceprice.service.PriceRuleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -33,6 +30,29 @@ public class PriceRuleController {
     @PostMapping("/edit")
     public ResponseResult edit(@RequestBody PriceRule priceRule) {
         return priceRuleService.edit(priceRule);
+    }
+
+    /**
+     * 查询最新的计价规则
+     *
+     * @param fareType
+     * @return
+     */
+    @GetMapping("/get-newest-version")
+    public ResponseResult<PriceRule> getNewestVersion(@RequestParam String fareType) {
+        return priceRuleService.getNewestVersion(fareType);
+    }
+
+    /**
+     * 判断规则是否是最新
+     *
+     * @param fareType
+     * @param fareVersion
+     * @return
+     */
+    @GetMapping("/is-new")
+    public ResponseResult<Boolean> isNew(@RequestParam String fareType, @RequestParam Integer fareVersion) {
+        return priceRuleService.isNew(fareType, fareVersion);
     }
 
 
