caa48d65ca615c68851ad972067867544b189b79
seekpan <lixin_qiu@163.com>
2024-01-27 10:27:30 +0800

    测试xml中自定义sql语句

--- online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/TestController.java
+++ online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.mashibing.servicedriveruser.controller;
 
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.servicedriveruser.mapper.DriverUserMapper;
 import com.mashibing.servicedriveruser.service.DriverUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,10 @@ public class TestController {
     @Autowired
     private DriverUserService driverUserService;
 
+    // 测试mapper中的xml是否正常使用
+    @Autowired
+    private DriverUserMapper driverUserMapper;
+
 
     @RequestMapping("/test")
     public String test() {
@@ -22,5 +27,11 @@ public class TestController {
     @GetMapping("test-db")
     public ResponseResult testDb() {
         return driverUserService.testGetDriverUser();
+    }
+
+    @GetMapping("/test-xml")
+    public int testXml(String cityCode) {
+        int i = driverUserMapper.selectDriverUserCountByCityCode(cityCode);
+        return i;
     }
 }
--- online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/mapper/DriverUserMapper.java
+++ online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/mapper/DriverUserMapper.java
@@ -3,7 +3,10 @@ package com.mashibing.servicedriveruser.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mashibing.internalcommon.dto.DriverUser;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface DriverUserMapper extends BaseMapper<DriverUser> {
+
+    public int selectDriverUserCountByCityCode(@RequestParam String cityCode);
 }
