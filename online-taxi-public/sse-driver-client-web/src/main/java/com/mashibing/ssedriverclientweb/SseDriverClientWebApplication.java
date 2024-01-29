a9f7c7b220390d2e14c1a538f6541e07abfa8bb3
seekpan <lixin_qiu@163.com>
2024-01-29 16:46:29 +0800

    模拟司机客户端的开发

---
+++ online-taxi-public/sse-driver-client-web/src/main/java/com/mashibing/ssedriverclientweb/SseDriverClientWebApplication.java
@@ -0,0 +1,13 @@
+package com.mashibing.ssedriverclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseDriverClientWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseDriverClientWebApplication.class, args);
+    }
+
+}
