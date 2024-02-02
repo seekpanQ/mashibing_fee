e1a9ede42cfcab7f6cb2b516e6d89c9500acb21f
seekpan <lixin_qiu@163.com>
2024-02-02 10:42:50 +0800

    订单未派单成功，订单无效编码完成

--- online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/dto/OrderConstants.java
+++ online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/dto/OrderConstants.java
@@ -1,7 +1,8 @@
 package com.mashibing.internalcommon.dto;
 
 public class OrderConstants {
-
+    // 0：订单无效
+    public static final int ORDER_INVALID = 0;
     // 1：订单开始
     public static final int ORDER_START = 1;
     // 2：司机接单
