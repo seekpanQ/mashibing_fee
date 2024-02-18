8d3d7783afb7b99c0d1c18e260899e5fe931abdb
seekpan <lixin_qiu@163.com>
2024-02-18 09:58:39 +0800

    调整maven目录，将internal-common独立出来和parent平级-代码修改

--- online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/util/JwtUtils.java
+++ online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/util/JwtUtils.java
@@ -4,13 +4,9 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.Verification;
 import com.mashibing.internalcommon.dto.TokenResult;
-import org.apache.commons.lang.time.DateUtils;
 
-import java.time.LocalDateTime;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
