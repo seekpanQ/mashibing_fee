b1704477220afdd97bceb268b35f028828935f14
seekpan <lixin_qiu@163.com>
2024-02-18 17:04:53 +0800

    不用“乘客发送和校验验证码的分组功能”

--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
@@ -1,13 +1,9 @@
 package com.mashibing.apipassenger.controller;
 
-import com.mashibing.apipassenger.constraints.CheckVerificationCodeGroup;
-import com.mashibing.apipassenger.constraints.SendVerificationCodeGroup;
 import com.mashibing.apipassenger.request.CheckVerificationCodeDTO;
 import com.mashibing.apipassenger.request.SendVerificationCodeDTO;
-import com.mashibing.apipassenger.request.TestSendVerificationCodeDTO;
 import com.mashibing.apipassenger.service.VerificationCodeService;
 import com.mashibing.internalcommon.dto.ResponseResult;
-import org.aspectj.weaver.ast.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,14 +17,14 @@ public class VerificationCodeController {
     private VerificationCodeService verificationCodeService;
 
     @PostMapping("/verification-code")
-    public ResponseResult verificationCode(@Validated(SendVerificationCodeGroup.class) @RequestBody TestSendVerificationCodeDTO verificationCodeDTO) {
+    public ResponseResult verificationCode(@Validated @RequestBody SendVerificationCodeDTO verificationCodeDTO) {
 
         String passengerPhone = verificationCodeDTO.getPassengerPhone();
         return verificationCodeService.generateCode(passengerPhone);
     }
 
     @PostMapping("/verification-code-check")
-    public ResponseResult checkVerificationCode(@Validated(CheckVerificationCodeGroup.class) @RequestBody TestSendVerificationCodeDTO checkVerificationCodeDTO) {
+    public ResponseResult checkVerificationCode(@Validated @RequestBody CheckVerificationCodeDTO checkVerificationCodeDTO) {
 
         String passengerPhone = checkVerificationCodeDTO.getPassengerPhone();
         String verificationCode = checkVerificationCodeDTO.getVerificationCode();
--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/request/SendVerificationCodeDTO.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/request/SendVerificationCodeDTO.java
@@ -12,8 +12,4 @@ public class SendVerificationCodeDTO {
     @NotBlank(message = "手机号不能为空")
     @Pattern(regexp = "^1[3,4,5,6,7,8,9]\\d{9}$", message = "请填写正确的手机号")
     private String passengerPhone;
-
-    private String verificationCode;
-
-    private String driverPhone;
 }
