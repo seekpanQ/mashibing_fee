4747374daf7ecd2a274b724fd61877d43ed548ca
seekpan <lixin_qiu@163.com>
2024-02-20 16:13:30 +0800

    并发编程三大特性-原子性(CAS)

---
+++ concurrent-programming/three-features/src/main/java/com/mashibing/ABA.java
@@ -0,0 +1,34 @@
+package com.mashibing;
+
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * **CAS的问题**：
+ * <p>
+ * * **ABA问题**：问题如下，可以引入版本号的方式，来解决ABA的问题。Java中提供了一个类在CAS时，针对各个版本追加版本号的操作。 AtomicStampeReference
+ * AtomicStampedReference在CAS时，不但会判断原值，还会比较版本信息。
+ * <p>
+ * **自旋时间过长问题**：
+ * <p>
+ * * 可以指定CAS一共循环多少次，如果超过这个次数，直接失败/或者挂起线程。（自旋锁、自适应自旋锁）
+ * * 可以在CAS一次失败后，将这个操作暂存起来，后面需要获取结果时，将暂存的操作全部执行，再返回最后的结果。
+ */
+public class ABA {
+
+    public static void main(String[] args) {
+        AtomicStampedReference<String> reference = new AtomicStampedReference<>("AAA", 1);
+
+        String oldValue = reference.getReference();
+        int oldVersion = reference.getStamp();
+        System.out.println("版本:" + oldVersion);
+
+        boolean b = reference.compareAndSet(oldValue, "B", oldVersion, oldVersion + 1);
+        System.out.println("版本:" + reference.getStamp());
+        System.out.println("修改1版本的：" + b);
+
+
+        boolean c = reference.compareAndSet("B", "C", 1, 1 + 1);
+        System.out.println("版本:" + reference.getStamp());
+        System.out.println("修改2版本的：" + c);
+    }
+}
---
+++ concurrent-programming/three-features/src/main/java/com/mashibing/CAS.java
@@ -0,0 +1,39 @@
+package com.mashibing;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 到底什么是CAS
+ * <p>
+ * compare and swap也就是比较和交换，他是一条CPU的并发原语。
+ * <p>
+ * 他在替换内存的某个位置的值时，首先查看内存中的值与预期值是否一致，如果一致，执行替换操作。这个操作是一个原子性操作。
+ * <p>
+ * Java中基于Unsafe的类提供了对CAS的操作的方法，JVM会帮助我们将方法实现CAS汇编指令。
+ * <p>
+ * 但是要清楚CAS只是比较和交换，在获取原值的这个操作上，需要你自己实现。
+ * <p>
+ * **CAS的缺点**：CAS只能保证对一个变量的操作是原子性的，无法实现对多行代码实现原子性。
+ */
+public class CAS {
+
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                count.incrementAndGet();
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                count.incrementAndGet();
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(count);
+    }
+}
